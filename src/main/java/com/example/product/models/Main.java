7ac1968c9dc1d34e92405eb1ffd7e13b7e135e13
dat35 <dotrungdat.mda@gmail.com>
2024-01-07 20:45:28 +0700

    2024-01-07

--- src/main/java/com/example/product/models/Main.java
+++ src/main/java/com/example/product/models/Main.java
@@ -1,19 +1,18 @@
 package com.example.product.models;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
-class Person{
+class Person {
     private String name;
     private String address;
 
 }
-class Teacher extends Person{
+
+class Teacher extends Person {
     private int id;
     private String salary;
-    public Teacher( int  id,String salary) {
+
+    public Teacher(int id, String salary) {
         this.id = id;
         this.salary = salary;
     }
@@ -31,20 +30,48 @@ class Teacher extends Person{
                 '}';
     }
 }
-class Student extends Person{
+
+class Student extends Person {
     private int credits = 0;
 
+    public Student(int credits) {
+        this.credits = credits;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
     @Override
-    public boolean equals(Object object){
-        if(object instanceof Student){
-            return true ;
-        }
-        return false;
+    public boolean equals(Object object) {
+        return (object instanceof Student) ? (((Student) object).getCredits() == this.credits) : false;
     }
 
 }
+
 public class Main {
     public static void main(String[] args) {
+        Student s1 = new Student(1);
+        Student s2 = new Student(11);
+        Student s3 = new Student(121);
+        List<Student> list = new ArrayList<>();
+        list = new LinkedList<>();
+        list.add(s1);
+        list.add(s2);
+        list.add(s1);
+        list.add(s3);
+        list.forEach(x ->{
+            System.out.println(x.getCredits());
+        });
+        list.remove(new Student(1112));
+        System.out.println("--------------");
+        list.forEach(x ->{
+            System.out.println(x.getCredits());
+        });
 //Equals
 //        Teacher ada = new Teacher();
 //        Person person  = new Person();
@@ -67,9 +94,13 @@ public class Main {
 //        int x1 = 10;
 //        Integer x2 = 10;
 //        System.out.println(x1==x2);
- // Trong bộ nhớ Ram phân thành 2 phân vùng chính là Stack và Heap
+        // Trong bộ nhớ Ram phân thành 2 phân vùng chính là Stack và Heap
 // Bộ nhớ stack có bộ nhớ nhỏ hơn dùng để lưu biến cục bộ, các hàm, hoặc các biến tham chiếu
 // Bộ nhớ heap chỉ dùng để lưu đối tượng, khi ta dùng từ khóa new đối tượng sẽ đc lưu vào bộ nhớ heap
         // khi các đối tượng k được sử dụng java sẽ tự động xóa các đối tượng đó trong heap.
+
+// LinkedList phù hợp trong th thêm sửa xóa còn lưu dữ liệu dùng  ArrayList
+        // Nên dùng cách sử dụng interface để trỏ đến đối tượng con (List<> list = new ArrayList<()) thể hiện tính đa hình vì nó không chỉ rõ là dùng class nào;
+        // Trong Vector mọi hàm đều là đồng bộ(chờ nhau để chạy), chươngtrinhfh sẽ bị chạy chậm nhưng bảo mật cao
     }
 }
